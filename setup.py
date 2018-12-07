56e511670abf58df970dede0edf1ebd7b75357cb
Tinywan <756684177@qq.com>
2018-12-07 15:09:45 +0800

    https://github.com/Tinywan/t_log

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setuptools.setup(
     name="t_log",
-    version="0.4",
+    version="0.5",
     author="Tinywan",
     author_email="756684177@qq.com",
     description="A small log package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Tinywan/log",
+    url="https://github.com/Tinywan/t_log",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
